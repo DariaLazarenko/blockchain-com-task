b4c6fddd7129666c42961bbc3aa872f10870741f
Daria Lazarenko <lazarenko200454@gmail.com>
2024-10-30 14:48:14 +0000

    Add coin details client component

--- app/coin/[id]/page.tsx
+++ app/coin/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { getCoinDetails } from "@/actions/getCoinDetails";
+import CoinDetails from "@/components/CoinDetails";
 import EmptyState from "@/components/EmptyState";
-import PriceInfoItem from "@/components/PriceInfoItem";
 import { Coin } from "@/models/coin";
 
 interface Props {
@@ -47,8 +47,6 @@ export async function generateMetadata({ params }: Props) {
 export default async function CoinDetailsPage({ params }: Props) {
   // New data will be fetched each time user loads a page because it is a dymanic route
   const coinDetails: Coin = await getCoinDetails(params.id);
-  const currentDateTime =
-    new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString();
 
   if (!coinDetails) {
     return (
@@ -58,38 +56,5 @@ export default async function CoinDetailsPage({ params }: Props) {
     );
   }
 
-  return (
-    <div className="p-8 flex justify-center">
-      <div className="max-w-sm sm:max-w-lg">
-        <h1 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-4 text-center">
-          {coinDetails.name}
-        </h1>
-        <h2 className="text-xl sm:text-2xl font-bold mb-4 sm:mb-6 text-center">
-          Last fetched {currentDateTime}
-        </h2>
-
-        <div className="bg-white shadow rounded-lg p-4 sm:p-6">
-          <div className="flex flex-col gap-6">
-            <PriceInfoItem
-              label="Current Price"
-              value={coinDetails.usdPrice}
-              prefix="$"
-            />
-
-            <PriceInfoItem
-              label="24h High"
-              value={coinDetails.usd24hHigh}
-              prefix="$"
-            />
-
-            <PriceInfoItem
-              label="24h Low"
-              value={coinDetails.usd24hLow}
-              prefix="$"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <CoinDetails coinId={params.id} initialData={coinDetails} />;
 }
---
+++ components/CoinDetails.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Coin } from "@/models/coin";
+import { useEffect, useState } from "react";
+import PriceInfoItem from "./PriceInfoItem";
+import { getCoinDetails } from "@/actions/getCoinDetails";
+import EmptyState from "./EmptyState";
+
+const TIMEOUT_S = 60;
+const ONE_SECOND = 1000;
+const TIMEOUT_MS = TIMEOUT_S * ONE_SECOND;
+
+interface Props {
+  coinId: string;
+  initialData: Coin;
+}
+
+export default function CoinDetails({ coinId, initialData }: Props) {
+  const [data, setData] = useState(initialData);
+  const [refetching, setRefetching] = useState(false);
+  const [timeUntilRefetch, setTimeUntilRefetch] = useState(TIMEOUT_S);
+
+  useEffect(() => {
+    const fetchInterval = setInterval(async () => {
+      setRefetching(true);
+      const response = await getCoinDetails(coinId);
+      setData(response);
+      setRefetching(false);
+      setTimeUntilRefetch(TIMEOUT_S);
+    }, TIMEOUT_MS);
+
+    const timerInterval = setInterval(() => {
+      setTimeUntilRefetch((prev) => (prev > 0 ? prev - 1 : 0));
+    }, ONE_SECOND);
+
+    return () => {
+      clearInterval(fetchInterval);
+      clearInterval(timerInterval);
+    };
+  }, [coinId]);
+
+  if (!data) {
+    return (
+      <div className="flex flex-1 justify-center items-center">
+        <EmptyState />
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-8 flex justify-center">
+      <div className="flex flex-1 flex-col max-w-sm sm:max-w-lg">
+        <div className="flex flex-1 flex-col sm:flex-row justify-between sm:items-center mb-2">
+          <h1 className="text-xl sm:text-2xl font-bold text-center">
+            {data.name}
+          </h1>
+          <div className="flex justify-center gap-1">
+            <p className="text-md text-gray-600">Will update in</p>
+            <p className="text-md text-gray-600 w-8">{timeUntilRefetch}s</p>
+          </div>
+        </div>
+
+        <div className="bg-white shadow rounded-lg p-4 sm:p-6">
+          <div className="flex flex-col gap-6">
+            <PriceInfoItem
+              label="Current Price"
+              value={data.usdPrice}
+              prefix="$"
+            />
+            <PriceInfoItem
+              label="24h High"
+              value={data.usd24hHigh}
+              prefix="$"
+            />
+            <PriceInfoItem label="24h Low" value={data.usd24hLow} prefix="$" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
