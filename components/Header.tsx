0576780987bfa3162fcfe31abe7e89e5170439f7
Daria Lazarenko <lazarenko200454@gmail.com>
2024-10-30 18:17:36 +0000

    Add navigation link to the main page in the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="bg-white shadow-md">
-      <div className="p-4 text-black text-lg">Blockchain.com</div>
+    <header className="bg-white shadow-md p-4">
+      <Link
+        href={`/`}
+        className="text-black text-lg"
+        aria-label={`Go to main page`}
+      >
+        Blockchain.com
+      </Link>
     </header>
   );
 }
