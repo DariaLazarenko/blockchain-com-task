75cdc994b99a4e3ae5e9d1f391f77c296c64f9ac
Daria Lazarenko <lazarenko200454@gmail.com>
2024-10-30 13:09:26 +0000

    Add message property for EmptyState component

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,7 +1,11 @@
-export default function EmptyState() {
+interface Props {
+  message?: string;
+}
+
+export default function EmptyState({ message = "No data available" }: Props) {
   return (
     <div className="flex flex-1 flex-col justify-center items-center text-gray-600">
-      <p className="text-lg">No data available</p>
+      <p className="text-lg">{message}</p>
     </div>
   );
 }
